30c593cb07ddcba1a8a5dae93dc2770171c774df
Tatyana <87974189+TatyanaGabdullina@users.noreply.github.com>
2026-04-05 20:39:35 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -56,7 +56,7 @@ export default async function handler(req, res) {
           userData: {
             firstName: "Test",
             lastName: "User",
-            email: "test@test.com"
+            email: "agrumisk@gmail.com"
           },
           cardDetail: {
     cardHolder: "Test User",
