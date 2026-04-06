4d23e0a07aff298d8ef4acd47fa5171a5133cd85
Tatyana <87974189+TatyanaGabdullina@users.noreply.github.com>
2026-04-06 15:38:46 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -16,14 +16,19 @@ export default async function handler(req, res) {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: `grant_type=client_credentials&client_id=l7233dc796764741eea9371f48353e0e0e&client_secret=ec2379668d9d4a00ba5000e007852634&scope=TATRAPAYPLUS`
+        body: new URLSearchParams({
+          grant_type: "client_credentials",
+          client_id: "l7233dc796764741eea9371f48353e0e0e",
+          client_secret: "ec2379668d9d4a00ba5000e007852634",
+          scope: "TATRAPAYPLUS"
+        })
       }
     );
 
     const tokenData = await tokenResponse.json();
     const accessToken = tokenData.access_token;
 
-    // 2. Создаём платёж
+    // 2. Создаём платёж (МИНИМАЛЬНЫЙ ВАРИАНТ)
     const paymentResponse = await fetch(
       "https://api.tatrabanka.sk/tatrapayplus/sandbox/v1/payments",
       {
@@ -33,43 +38,30 @@ export default async function handler(req, res) {
           "Content-Type": "application/json",
           Accept: "application/json",
           "X-Request-ID": crypto.randomUUID(),
-          "IP-Address": "8.8.8.8",
-          "Redirect-URI": "https://jenyberg.com/dakujeme"
+          "IP-Address": req.headers["x-forwarded-for"] || "8.8.8.8",
+          "Redirect-URI": "https://jenyberg.com/dakujeme",
+          "Preferred-Method": "CARD_PAY"
         },
-        body: `{
-          "basePayment": {
-            "instructedAmount": {
-              "amountValue": 1,
-              "currency": "EUR"
-            },
-            "endToEnd": {
-              "variableSymbol": "123",
-              "specificSymbol": "123",
-              "constantSymbol": "0308"
+        body: JSON.stringify({
+          basePayment: {
+            instructedAmount: {
+              amountValue: 1,
+              currency: "EUR"
             }
           },
-          "userData": {
-            "firstName": "Test",
-            "lastName": "User",
-            "email": "test@test.com"
-          },
-          "cardDetail": {
-            "cardHolder": "Test User",
-            "billingAddress": {
-              "streetName": "Test Street",
-              "buildingNumber": "1",
-              "townName": "Bratislava",
-              "postCode": "81101",
-              "country": "SK"
-            }
+          redirectUrls: {
+            success: "https://jenyberg.com/dakujeme",
+            cancel: "https://jenyberg.com/dakujeme",
+            fail: "https://jenyberg.com/dakujeme"
           }
-        }`
+        })
       }
     );
 
     const data = await paymentResponse.json();
 
     return res.status(200).json({
+      status: "success",
       payment_url: data.tatraPayPlusUrl || null,
       debug: data
     });
