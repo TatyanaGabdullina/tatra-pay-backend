ab9928da8620c5ea7ea21f6066f30a59ed8cecf1
Tatyana <87974189+TatyanaGabdullina@users.noreply.github.com>
2026-04-06 13:29:40 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -76,7 +76,11 @@ export default async function handler(req, res) {
 
     const data = await paymentResponse.json();
 
-    return res.status(200).send(JSON.stringify(data, null, 2));
+    return res.status(200).json({
+      status: "success",
+      payment_url: data.tatraPayPlusUrl || null,
+      debug: data
+    });
 
   } catch (error) {
     return res.status(500).json({
