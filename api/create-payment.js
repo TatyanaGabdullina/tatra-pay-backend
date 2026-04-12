0854484ce5e461a1cf7aab51548a23690d6f8f22
Tatyana <87974189+TatyanaGabdullina@users.noreply.github.com>
2026-04-12 17:55:24 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
   if (req.method === "GET") {
     return res.status(200).json({ test: "ok" });
@@ -8,71 +10,62 @@ export default async function handler(req, res) {
   }
 
   try {
-    const tokenResponse = await fetch(
+    // TOKEN
+    const tokenResponse = await axios.post(
       "https://api.tatrabanka.sk/tatrapayplus/auth/oauth/v2/token",
+      "grant_type=client_credentials&client_id=l7233dc796764741eea9371f48353e0e0e&client_secret=ec2379668d9d4a00ba5000e007852634&scope=TATRAPAYPLUS",
       {
-        method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: new URLSearchParams({
-          grant_type: "client_credentials",
-          client_id: "l7233dc796764741eea9371f48353e0e0e",
-          client_secret: "ec2379668d9d4a00ba5000e007852634",
-          scope: "TATRAPAYPLUS"
-        })
+        }
       }
     );
 
-    const tokenData = await tokenResponse.json();
-    const accessToken = tokenData.access_token;
+    const accessToken = tokenResponse.data.access_token;
 
-    const paymentResponse = await fetch(
+    // PAYMENT
+    const paymentResponse = await axios.post(
       "https://api.tatrabanka.sk/tatrapayplus/v1/payments",
       {
-        method: "POST",
+        basePayment: {
+          instructedAmount: {
+            amountValue: 1,
+            currency: "EUR"
+          },
+          endToEnd: {
+            variableSymbol: "123",
+            specificSymbol: "123",
+            constantSymbol: "0308"
+          }
+        },
+        userData: {
+          firstName: "Test",
+          lastName: "User",
+          email: "agrumisk@gmail.com"
+        }
+      },
+      {
         headers: {
           Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
           Accept: "application/json",
+          "Content-Type": "application/json",
           "X-Request-ID": crypto.randomUUID(),
           "IP-Address": "1.1.1.1",
           "Redirect-URI": "https://jenyberg.com/dakujeme",
           "Preferred-Method": "CARD_PAY"
-        },
-        body: JSON.stringify({
-          basePayment: {
-            instructedAmount: {
-              amountValue: 1,
-              currency: "EUR"
-            },
-            endToEnd: {
-              variableSymbol: "123",
-              specificSymbol: "123",
-              constantSymbol: "0308"
-            }
-          },
-          userData: {
-            firstName: "Test",
-            lastName: "User",
-            email: "agrumisk@gmail.com"
-          },
-          
-        })
+        }
       }
     );
 
-    const data = await paymentResponse.json();
-
     return res.status(200).json({
-      status: "success",
-      payment_url: data.tatraPayPlusUrl || null,
-      debug: data
+      payment_url: paymentResponse.data.tatraPayPlusUrl,
+      debug: paymentResponse.data
     });
 
   } catch (error) {
     return res.status(500).json({
-      error: error.message
+      error: error.message,
+      details: error.response?.data || null
     });
   }
 }
